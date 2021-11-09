f0b08599f0e45be559dde2c96c2c8798d0be282e
Melanie Ann Toledo <melanieanntoledo@yahoo.com>
2021-11-09 15:47:50 -0600

    297.DOM changes

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -9,12 +9,14 @@ class ResultsView extends View {
     _generateMarkup() {
         console.log(this.data);
         return this._data.map(this._generateMarkupPreview).join('');
-    }
+    };
 
     _generateMarkupPreview(result) {
+        const id = window.location.hash.slice(1);
+
         return `
             <li class="preview">
-                <a class="preview__link" href="#${result.id}">
+                <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
                 <figure class="preview__fig">
                     <img crossorigin src="${result.image}" alt="${result.title}" />
                 </figure>
@@ -25,7 +27,7 @@ class ResultsView extends View {
                 </a>
             </li>
         `;
-    }
+    };
 }
 
 export default new ResultsView();
